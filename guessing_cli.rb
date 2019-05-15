1d85a57335fe045af2ae6e00a066ce5556b6f2ba
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-15 00:14:43 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,7 @@
 # Code your solution here!
 
 def end_app
+#<<<<<<< HEAD
     puts "Goodbye"
     exit
 end
@@ -45,6 +46,25 @@ def run_guessing_game
   puts "!-- Welcome to the Guessing Game --!"
   turn
   
+#=======
+end
+
+
+
+def run_guessing_game
+  puts "Welcome to the Guessing Game"
+  puts "Guess a number between 1 - 10"
+
+  inp = gets.chomp
+  res = rand(1..10)
+  
+  if inp == res.to_s
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{inp.to_i}"
+  end
+
+#>>>>>>> adcfa2b2a7e6aa72ac961a72c3390b5b2c6f64a8
 end
 
 run_guessing_game
